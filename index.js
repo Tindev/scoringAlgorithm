0034cbdb59248a6d5c67dfb05ba1c96a3854a354
Gijs Weterings <gijsweterings+github@gmail.com>
2017-04-12 21:55:29 +0200

    logscale and static scale on the calculated_skill to get it around 0-1 scale

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ async function scoreUsers() {
     let body = {
         script: {
             lang: "painless",
-            inline: "if (ctx._source.containsKey('contributions')) { ctx._source.calculated_skill = 0.05 * ctx._source.followers + 0.05 * ctx._source.public_repos + 0.05 * ctx._source.contributions } else { ctx._source.calculated_skill = 0 }"
+            inline: "if (ctx._source.containsKey('contributions')) { ctx._source.calculated_skill = Math.log(0.05 * ctx._source.followers + 0.05 * ctx._source.public_repos + 0.05 * ctx._source.contributions) / 6  } else { ctx._source.calculated_skill = 0 }"
         }
     }
 
     try {
-        let queryPromise = await fetch('http://tindev.gijsweterings.nl/es/users/_update_by_query', 
+        let queryPromise = await fetch('http://tindev.gijsweterings.nl/es/backusers/_update_by_query', 
         {
             method: "POST",
             headers: {
